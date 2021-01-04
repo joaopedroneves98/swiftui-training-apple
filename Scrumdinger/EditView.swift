f8be44b0b534d8574a94971bc752c8026c2e4682
João Neves <42605490+joaopedroneves98@users.noreply.github.com>
2021-01-04 15:40:29 +0000

    Creating the edit view

---
+++ Scrumdinger/EditView.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+
+struct EditView: View {
+    @State private var scrumData: DailyScrum.Data = DailyScrum.Data()
+    @State private var newAttendee = ""
+    var body: some View {
+        List {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrumData.title)
+                HStack {
+                    Slider(value: $scrumData.lengthInMinutes, in: 5...30, step: 1.0) {
+                        Text("Length")
+                    }
+                    Spacer()
+                    Text("\(Int(scrumData.lengthInMinutes)) minutes")
+                }
+                ColorPicker("Color", selection: $scrumData.color)
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(scrumData.attendees, id: \.self) { attendee in
+                    Text(attendee)
+                }
+                .onDelete { indices in
+                    scrumData.attendees.remove(atOffsets: indices)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendee)
+                    Button(action: {
+                        withAnimation {
+                            scrumData.attendees.append(newAttendee)
+                            newAttendee = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView()
+    }
+}
--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -26,3 +26,16 @@ extension DailyScrum {
         ]
     }
 }
+
+extension DailyScrum {
+    struct Data {
+        var title: String = ""
+        var attendees: [String] = []
+        var lengthInMinutes: Double = 5.0
+        var color: Color = Color.white
+        
+        var data: Data {
+            return Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), color: color)
+        }
+    }
+}
